44727221028493e2b85fea7fea7ce239e81eeb3a
Risto Korhonen <risto.korhonen@student.oulu.fi>
2023-11-21 10:24:56 +0200

    Fix IPv6 connectivity through border router, change sensors from thread to shell handler (temp. fix)

--- node/main.c
+++ node/main.c
@@ -8,50 +8,33 @@
 #include "mutex.h"
 #include "msg.h"
 
-#define SENSOR_PERIOD_MS    5000
-#define INIT_SLEEP          5000
-
 // Global variables
 static lpsxxx_t lpsxxx;
 
-static char sensor_stack[THREAD_STACKSIZE_MAIN];
-
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
-
-// Mutex to protect global variables
-mutex_t sensor_mutex;
-
-static int16_t g_temperature;
-static uint16_t g_pressure;
 
 static int sensor_handler(int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
 
-    mutex_lock(&sensor_mutex);
-    printf("Temperature %i.%u°C, Pressure %uhPa \n", g_temperature / 100, 
-            g_temperature % 100, g_pressure);
-    mutex_unlock(&sensor_mutex);
-    return 0;
-}
+    int16_t temperature = 0;
+    uint16_t pressure = 0;
 
-static void* sensor_thread(void *arg)
-{
-    (void) arg;
-
-    while(1)
+    if (lpsxxx_read_temp(&lpsxxx, &temperature) != LPSXXX_OK)
     {
-        mutex_lock(&sensor_mutex);
-        lpsxxx_read_temp(&lpsxxx, &g_temperature);
-        lpsxxx_read_pres(&lpsxxx, &g_pressure);
-        mutex_unlock(&sensor_mutex);
-
-        ztimer_sleep(ZTIMER_MSEC, SENSOR_PERIOD_MS);
+        puts("LPS331AP temperature read failed");
     }
 
-    return NULL; // Should never get there
+    if (lpsxxx_read_pres(&lpsxxx, &pressure) != LPSXXX_OK)
+    {
+       puts("LPS331AP pressure read failed");
+    }
+
+    printf("Temperature %i.%u°C, Pressure %uhPa \n", temperature / 100, 
+            temperature % 100, pressure);
+    return 0;
 }
 
 static const shell_command_t shell_commands[] =
@@ -62,15 +45,13 @@ static const shell_command_t shell_commands[] =
 
 int main(void)
 {
-    // Just have enough sleep to get the first print also shown to terminal
-    ztimer_sleep(ZTIMER_MSEC, INIT_SLEEP);
     puts("Application starts");
 
-    lpsxxx_init(&lpsxxx, &lpsxxx_params[0]);
+    if (lpsxxx_init(&lpsxxx, &lpsxxx_params[0]) != LPSXXX_OK)
+    {
+        puts("LPS331AP init failed");
+    }
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-
-    thread_create(sensor_stack, sizeof(sensor_stack), THREAD_PRIORITY_MAIN - 1,
-                  0, sensor_thread, NULL, "Sensor thread");
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
