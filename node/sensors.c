6e86e143a4924562cc995cd7dc4cd0076cc1d3d2
Risto Korhonen <risto.korhonen@student.oulu.fi>
2023-11-30 12:21:14 +0200

    Add optional sensor sw reset to sensors_init

--- node/sensors.c
+++ node/sensors.c
@@ -2,6 +2,9 @@
 #include "lpsxxx_params.h"
 #include "sensors.h"
 
+#define LPS331AP_REG_RES_CONF           0x10
+#define LPS331AP_REG_CTRL_REG2          0x21
+
 // Global variables
 static lpsxxx_t lpsxxx;
 
@@ -11,6 +14,23 @@ int sensors_init(void)
     {
         return 1;
     }
+
+#if 1
+    i2c_acquire(lpsxxx.params.i2c);
+    if (i2c_write_reg(lpsxxx.params.i2c, lpsxxx.params.addr, 
+                      LPS331AP_REG_CTRL_REG2, 0x84, 0) < 0)
+    {
+        i2c_release(lpsxxx.params.i2c);
+        return 1;
+    }
+    i2c_release(lpsxxx.params.i2c);
+#endif
+
+    if (lpsxxx_enable(&lpsxxx) != LPSXXX_OK)
+    {
+        return 1;
+    }
+
     return 0;
 }
 
